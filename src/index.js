da67a60f565e6d31d6537e07a4f61992fa25f8e8
SashaKurban <sasha_kurbanova@yahoo.com>
2022-08-18 13:35:13 -0400

    added education and skills sections

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-document.addEventListener('DOMContentLoaded', navFunction)
 let mobileNavOpen = false;
+
 
 function navFunction(){
     const nav = document.querySelector('.nav-bar');
     nav.classList.toggle('show');
+    document.body.classList.toggle("no-scroll");
     mobileNavOpen = !mobileNavOpen;
 }
 
 function navLinkFunction(){
     const nav = document.querySelector('.nav-bar');
     if(mobileNavOpen){
+        document.body.classList.toggle("no-scroll");
         nav.classList.toggle('show');
         mobileNavOpen = !mobileNavOpen;
     }
